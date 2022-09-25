17556d92c03509fd380ddf329efd9fb3fad9ab1d
jhsharp <73131537+jhsharp@users.noreply.github.com>
2022-09-25 09:31:58 -0500

    Caveman Blinking :eyes:

---
+++ Assets/Scripts/SpriteManagement/BPlayerHead.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPlayerHead : MonoBehaviour
+{
+    public Animator headAnimator;
+    private Rigidbody playerBody;
+    private BPlayerControls playerControls;
+
+    void Start()
+    {
+        playerControls = GetComponentInParent<BPlayerControls>();
+        playerBody = playerControls.playerBody;
+    }
+
+    void Update()
+    {
+        headAnimator.SetBool("Moving", playerBody.velocity.magnitude > 0.25f || playerControls.grabbing);
+    }
+}
